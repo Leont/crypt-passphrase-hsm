e7b567ec83ba0a897bcec166c46a9dfea645d1b1
Leon Timmermans <fawaka@gmail.com>
2023-03-29 13:51:53 +0200

    Initial commit

---
+++ lib/Crypt/Passphrase/Pepper/HSM.pm
@@ -0,0 +1,35 @@
+package Crypt::Passphrase::Pepper::HSM;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Crypt::Passphrase::Pepper::HSM - INSERT YOUR ABSTRACT HERE
+
+=head1 DESCRIPTION
+
+Write a full description of the module and its features here.
+
+=head1 AUTHOR
+
+Leon Timmermans <leont@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2023 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
